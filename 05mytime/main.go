e0d16770aaa35786798a90065f26fca023df68c3
Vaibhav <72972732+vaibhavgits@users.noreply.github.com>
2024-07-20 16:50:57 +0530

    Added Time and Build files

---
+++ 05mytime/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study of golang")
+
+	fmt.Println("Present time w/o format is: ")
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println("Present time after formatting is: ")
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Mon"))
+
+	createdDate := time.Date(2020, time.August, 12, 23, 23, 0, 0, time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+}
